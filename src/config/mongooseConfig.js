1e0755ca5e85c54b1574fbdf64ccd5e42ece555d
rajatdeeporion <rajat.thakkar@deeporion.com>
2024-10-21 15:59:49 +0530

    change env

--- src/config/mongooseConfig.js
+++ src/config/mongooseConfig.js
@@ -8,23 +8,34 @@ dotenv.config();
 console.log(dotenv.config()); 
 // Access the environment variables defined in the .env file
 const url = process.env.MONGOURL; // MongoDB connection URL
-const port = process.env.PORT;      // Server port
+const port = process.env.PORT||3000 ;      // Server port
 // Logging the MongoDB URL and server port to the console
 console.log('MongoDB URL:', url);   // Output: MongoDB URL: mongodb://localhost:27017/placement_cell
 console.log('Server Port:', port);    // Output: Server Port: 3000
 // Function to create a connection to MongoDB using Mongoose
+// const createMongooseConection = async () => {
+//   try {
+//     // Attempting to connect to MongoDB
+//     await mongoose.connect(url, {
+//       useNewUrlParser: true,  // Ensures use of the new connection string parser.
+//       useUnifiedTopology: true // Enables stable connections using the unified topology.
+//     });
+//     console.log("Mongoose is connected"); // Log success message if connected
+//   } catch (error) {
+//     // Log any error encountered during the connection attempt
+//     console.log("got an error", error);
+//   }
+// };
+// // Exporting the createMongooseConection function for use in other modules
+// export default createMongooseConection; 
 const createMongooseConection = async () => {
-  try {
-    // Attempting to connect to MongoDB
-    await mongoose.connect(url, {
-      useNewUrlParser: true,  // Ensures use of the new connection string parser.
-      useUnifiedTopology: true // Enables stable connections using the unified topology.
-    });
-    console.log("Mongoose is connected"); // Log success message if connected
-  } catch (error) {
-    // Log any error encountered during the connection attempt
-    console.log("got an error", error);
-  }
+    try {
+        // Connecting to MongoDB using the connection string from the environment variable
+        console.log(url)
+        await mongoose.connect(url);
+        console.log("Mongoose is connected");
+    } catch (error) {
+        console.log("Got an error", error);
+    }
 };
-// Exporting the createMongooseConection function for use in other modules
-export default createMongooseConection; 
+export default createMongooseConection
