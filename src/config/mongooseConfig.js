d0c89c7a3a981f9a9ddb0b951c34a74936e9a87f
rajatdeeporion <rajat.thakkar@deeporion.com>
2024-10-21 16:21:57 +0530

    cleane code

--- src/config/mongooseConfig.js
+++ src/config/mongooseConfig.js
@@ -8,26 +8,7 @@ dotenv.config();
 console.log(dotenv.config()); 
 // Access the environment variables defined in the .env file
 const url = process.env.MONGOURL; // MongoDB connection URL
-// const port = process.env.PORT||3000 ;      // Server port
-// Logging the MongoDB URL and server port to the console
-// console.log('MongoDB URL:', url);   // Output: MongoDB URL: mongodb://localhost:27017/placement_cell
-// console.log('Server Port:', port);    // Output: Server Port: 3000
-// Function to create a connection to MongoDB using Mongoose
-// const createMongooseConection = async () => {
-//   try {
-//     // Attempting to connect to MongoDB
-//     await mongoose.connect(url, {
-//       useNewUrlParser: true,  // Ensures use of the new connection string parser.
-//       useUnifiedTopology: true // Enables stable connections using the unified topology.
-//     });
-//     console.log("Mongoose is connected"); // Log success message if connected
-//   } catch (error) {
-//     // Log any error encountered during the connection attempt
-//     console.log("got an error", error);
-//   }
-// };
-// // Exporting the createMongooseConection function for use in other modules
-// export default createMongooseConection; 
+
 const createMongooseConection = async () => {
     try {
         // Connecting to MongoDB using the connection string from the environment variable
